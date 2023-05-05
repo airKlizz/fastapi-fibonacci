a5027889bc2ae86fd3ff8852b38a50cc8594bdc7
Rémi Calizzano <remi.calizzano@wescale.fr>
2023-05-05 11:45:35 +0200

    add k6 grafana

--- stress_test.js
+++ stress_test.js
@@ -1,41 +1,84 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
 
 export let options = {
-    stages: [
-        { duration: '30s', target: 10 },
-        { duration: '1m', target: 50 },
-        { duration: '30s', target: 10 },
-        { duration: '1m', target: 0 },
-
-        { duration: '30s', target: 10 },
-        { duration: '1m', target: 50 },
-        { duration: '30s', target: 10 },
-        { duration: '1m', target: 0 },
-
-        { duration: '30s', target: 10 },
-        { duration: '1m', target: 50 },
-        { duration: '30s', target: 10 },
-        { duration: '1m', target: 0 },
-
-        { duration: '30s', target: 10 },
-        { duration: '1m', target: 50 },
-        { duration: '30s', target: 10 },
-    ],
+    scenarios: {
+        inefficient_fibonacci: {
+            exec: 'inefficient_fibonacci',
+            executor: 'ramping-arrival-rate',
+            startRate: 0,
+            timeUnit: '1s',
+            preAllocatedVUs: 50,
+            maxVUs: 100,
+            startTime: '0s',
+            stages: [
+                { duration: '30s', target: 10 },
+                { duration: '1m', target: 50 },
+                { duration: '30s', target: 10 },
+                { duration: '1m', target: 0 },
+            ],
+        },
+        efficient_fibonacci: {
+            exec: 'efficient_fibonacci',
+            executor: 'ramping-arrival-rate',
+            startRate: 0,
+            timeUnit: '1s',
+            preAllocatedVUs: 50,
+            maxVUs: 100,
+            startTime: '3m',
+            stages: [
+                { duration: '30s', target: 10 },
+                { duration: '1m', target: 50 },
+                { duration: '30s', target: 10 },
+                { duration: '1m', target: 0 },
+            ],
+        },
+        memoized_fibonacci: {
+            exec: 'memoized_fibonacci',
+            executor: 'ramping-arrival-rate',
+            startRate: 0,
+            timeUnit: '1s',
+            preAllocatedVUs: 50,
+            maxVUs: 100,
+            startTime: '6m',
+            stages: [
+                { duration: '30s', target: 10 },
+                { duration: '1m', target: 50 },
+                { duration: '30s', target: 10 },
+                { duration: '1m', target: 0 },
+            ],
+        },
+        inefficient_cpu_intensive_fibonacci: {
+            exec: 'inefficient_cpu_intensive_fibonacci',
+            executor: 'ramping-arrival-rate',
+            startRate: 0,
+            timeUnit: '1s',
+            preAllocatedVUs: 50,
+            maxVUs: 100,
+            startTime: '9m',
+            stages: [
+                { duration: '30s', target: 10 },
+                { duration: '1m', target: 50 },
+                { duration: '30s', target: 10 },
+            ],
+        },
+    },
 };
 
-export default function () {
-    const BASE_URL = 'http://fastapi:80';
-    const FIBONACCI_NUMBER = 20;
-    const stage = __VU * 3 + __ITER;
+const BASE_URL = 'http://localhost:4000';
+const FIBONACCI_NUMBER = 20;
 
-    if (stage % 12 < 3) {
-        http.get(`${BASE_URL}/inefficient_fibonacci/${FIBONACCI_NUMBER}`);
-    } else if (stage % 12 < 6) {
-        http.get(`${BASE_URL}/efficient_fibonacci/${FIBONACCI_NUMBER}`);
-    } else if (stage % 12 < 9) {
-        http.get(`${BASE_URL}/memoized_fibonacci/${FIBONACCI_NUMBER}`);
-    } else {
-        http.get(`${BASE_URL}/inefficient_cpu_intensive_fibonacci/${FIBONACCI_NUMBER}`);
-    }
+export function inefficient_fibonacci() {
+    http.get(`${BASE_URL}/inefficient_fibonacci/${FIBONACCI_NUMBER}`);
+}
+
+export function efficient_fibonacci() {
+    http.get(`${BASE_URL}/efficient_fibonacci/${FIBONACCI_NUMBER}`);
+}
+
+export function memoized_fibonacci() {
+    http.get(`${BASE_URL}/memoized_fibonacci/${FIBONACCI_NUMBER}`);
+}
+
+export function inefficient_cpu_intensive_fibonacci() {
+    http.get(`${BASE_URL}/inefficient_cpu_intensive_fibonacci/${FIBONACCI_NUMBER}`);
 }
