40f8aa5d8f05d1a36bb3ff2f864f00b84c0a7edc
Rémi Calizzano <remi.calizzano@wescale.fr>
2023-05-09 09:34:04 +0200

    feat: move stress test to constant vus

--- stress_test.js
+++ stress_test.js
@@ -1,84 +1,58 @@
 import http from 'k6/http';
+import { sleep } from 'k6';
 
-export let options = {
+export const options = {
     scenarios: {
         inefficient_fibonacci: {
             exec: 'inefficient_fibonacci',
-            executor: 'ramping-arrival-rate',
-            startRate: 0,
-            timeUnit: '1s',
-            preAllocatedVUs: 50,
-            maxVUs: 100,
-            startTime: '0s',
-            stages: [
-                { duration: '30s', target: 10 },
-                { duration: '1m', target: 50 },
-                { duration: '30s', target: 10 },
-                { duration: '1m', target: 0 },
-            ],
+            executor: 'constant-vus',
+            vus: 1000,
+            duration: '1m',
+            startTime: '1m',
         },
         efficient_fibonacci: {
             exec: 'efficient_fibonacci',
-            executor: 'ramping-arrival-rate',
-            startRate: 0,
-            timeUnit: '1s',
-            preAllocatedVUs: 50,
-            maxVUs: 100,
+            executor: 'constant-vus',
+            vus: 1000,
+            duration: '1m',
             startTime: '3m',
-            stages: [
-                { duration: '30s', target: 10 },
-                { duration: '1m', target: 50 },
-                { duration: '30s', target: 10 },
-                { duration: '1m', target: 0 },
-            ],
         },
         memoized_fibonacci: {
             exec: 'memoized_fibonacci',
-            executor: 'ramping-arrival-rate',
-            startRate: 0,
-            timeUnit: '1s',
-            preAllocatedVUs: 50,
-            maxVUs: 100,
-            startTime: '6m',
-            stages: [
-                { duration: '30s', target: 10 },
-                { duration: '1m', target: 50 },
-                { duration: '30s', target: 10 },
-                { duration: '1m', target: 0 },
-            ],
+            executor: 'constant-vus',
+            vus: 1000,
+            duration: '1m',
+            startTime: '5m',
         },
         inefficient_cpu_intensive_fibonacci: {
             exec: 'inefficient_cpu_intensive_fibonacci',
-            executor: 'ramping-arrival-rate',
-            startRate: 0,
-            timeUnit: '1s',
-            preAllocatedVUs: 50,
-            maxVUs: 100,
-            startTime: '9m',
-            stages: [
-                { duration: '30s', target: 10 },
-                { duration: '1m', target: 50 },
-                { duration: '30s', target: 10 },
-            ],
+            executor: 'constant-vus',
+            vus: 1000,
+            duration: '1m',
+            startTime: '7m',
         },
     },
 };
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = 'http://fastapi:80';
 const FIBONACCI_NUMBER = 20;
 
 export function inefficient_fibonacci() {
     http.get(`${BASE_URL}/inefficient_fibonacci/${FIBONACCI_NUMBER}`);
+    sleep(0.5);
 }
 
 export function efficient_fibonacci() {
     http.get(`${BASE_URL}/efficient_fibonacci/${FIBONACCI_NUMBER}`);
+    sleep(0.5);
 }
 
 export function memoized_fibonacci() {
     http.get(`${BASE_URL}/memoized_fibonacci/${FIBONACCI_NUMBER}`);
+    sleep(0.5);
 }
 
 export function inefficient_cpu_intensive_fibonacci() {
     http.get(`${BASE_URL}/inefficient_cpu_intensive_fibonacci/${FIBONACCI_NUMBER}`);
+    sleep(0.5);
 }
